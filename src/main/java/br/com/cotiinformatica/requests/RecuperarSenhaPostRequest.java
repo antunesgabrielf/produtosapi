e3da9b08a9ab65666be359aea717238900432e8f
antunesgabrielf <gabriel_antunes_20@hotmail.com>
2022-07-16 09:26:43 -0300

    Cadastro, autenticacao e recuperacao de senha

---
+++ src/main/java/br/com/cotiinformatica/requests/RecuperarSenhaPostRequest.java
@@ -0,0 +1,12 @@
+package br.com.cotiinformatica.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RecuperarSenhaPostRequest {
+
+	private String email;
+		
+}
